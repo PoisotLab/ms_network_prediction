049b2364f252f17fc39bfbbf8c3d9b1124f62566
Timothée Poisot <tpoisot@pop-os.localdomain>
2020-10-31 14:58:31 -0400

    update assets Closes Update assets for the webpage rendering #7

--- .assets/scripts/affiliations.py
+++ .assets/scripts/affiliations.py
@@ -16,13 +16,16 @@ for creator in creators:
             affiliation_to_number[affiliation] = len(affiliation_to_number)+1
 
 for creator in creators:
-    creator["affilcode"] = [affiliation_to_number[affiliation] for affiliation in creator["affiliations"]]
+    creator["affilcode"] = [affiliation_to_number[affiliation]
+                            for affiliation in creator["affiliations"]]
     creator.pop("affiliations", None)
 
 affiliations = {}
 affiliations["authors"] = creators
-affiliations["institutions"] = [{"id": v, "name": k} for k, v in sorted(affiliation_to_number.items(), key=lambda item: item[1])]
+affiliations["institutions"] = [{"id": v, "name": k} for k, v in sorted(
+    affiliation_to_number.items(), key=lambda item: item[1])]
 
 with open("affiliations.json", "w") as outfile:
-    json.dump(affiliations, outfile, sort_keys=False, indent=4, separators=(',', ': '))
+    json.dump(affiliations, outfile, sort_keys=False,
+              indent=4, separators=(',', ': '))
     outfile.write('\n')
